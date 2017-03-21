56a4abe81cec5c4dbaefed40fa08dd1615864c0c
John Gibson <jgibson2@users.noreply.github.com>
2017-03-21 10:16:45 -0500

    Add files via upload

---
+++ firmware/nonapus/PINDEF.h
@@ -0,0 +1,31 @@
+#ifndef __PINDEF_H__
+#define __PINDEF_H__
+
+//analog pins
+#define APIN1 A11
+#define APIN2 A10
+#define APIN3 20
+#define APIN4 19
+#define APIN5 18
+#define APIN6 17
+#define APIN7 16
+#define APIN8 15
+#define APIN9 14
+
+//pwm pins
+#define PWM1 5
+#define PWM2 6
+#define PWM3 9
+#define PWM4 21
+#define PWM5 4
+#define PWM6 10
+#define PWM7 23
+#define PWM8 22
+#define PWM9 3
+
+//Button Pins
+#define B0 2
+#define B1 7
+#define B2 0
+
+#endif
